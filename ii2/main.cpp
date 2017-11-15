393caf713d6640034c76168ed79e94c27f7b0ad4
razvanboboc <32697195+razvanboboc@users.noreply.github.com>
2017-11-15 09:36:11 +0200

    Update main.cpp

--- ii2/main.cpp
+++ ii2/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-
 using namespace std;
-int n,x,v[100],j,k,i,t;
+int n,j,k,i;
+float v[100],t;
 int main()
 {
     cin>>n;
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
         cin>>v[i];
-    for(j=2;j<=n;j+=2){
+    for(j=1;j<n;j+=2){
         t=v[j];
-        v[j]=(float)(t+v[j-1])/2;
+        v[j]=(t+v[j-1])/2;
         n++;
-        for(k=n;k>=j;k--)
+        for(k=n-1;k>j;k--)
             v[k]=v[k-1];
         v[j+1]=t;
     }
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
         cout<<v[i]<<" ";
     return 0;
 }
